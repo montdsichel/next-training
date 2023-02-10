5883756d1df7051c42b174d496b3e31197e2306f
CodeSandbox <support@codesandbox.io>
2023-02-10 05:15:44 +0000

    Initial commit

---
+++ src/components/pages/top/Top.page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Container from "~/components/pages/top/Top";
+
+export const View: React.FC = () => {
+  return (
+    <>
+      <Container />
+    </>
+  );
+};
---
+++ src/models/Sample.ts
@@ -0,0 +1,21 @@
+export interface SampleArray {
+  id: number;
+  name: string;
+}
+
+export const sampleArray = [
+  {
+    id: 1,
+    name: "1",
+  },
+  {
+    id: 2,
+    name: "2",
+  },
+  {
+    id: 3,
+    name: "3",
+  },
+];
+
+export const sampleArray2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
