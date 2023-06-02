38a5a18ef760503b8140a985bb37856aebfc4a75
M.Matsuda <montdsichel@gmail.com>
2023-06-02 10:39:02 +0000

    位置情報取得周りのロジック改修とsass導入

--- src/components/pages/top/Top.tsx
+++ src/components/pages/top/Top.tsx
@@ -1,48 +1,58 @@
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { GetGoogleMapApiKey } from "~/utils/Environment";
 import GoogleMapReact from "google-map-react";
-import styles from "~/styles/Top.module.css";
+import styles from "~/components/pages/top/Top.module.scss";
+
+interface Positions {
+  lat: number;
+  lng: number;
+}
 
 type Props = {
-  children: ReactNode;
+  apiKey: string;
+  defaultCenter: Positions | undefined;
+  defaultZoom: number;
 };
 
 const Component: React.FC<Props> = (props) => (
-  <div className={styles.mapWrapper}>{props.children}</div>
+  <div className={styles.mapWrapper}>
+    {props.defaultCenter && ( // 位置情報がundefined以外なら地図を描画
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: props.apiKey }}
+        defaultCenter={props.defaultCenter}
+        defaultZoom={props.defaultZoom}
+      />
+    )}
+  </div>
 );
 
-const Container: React.FC<Props> = () => {
-  const [position, setPosition] = useState({
-    latitude: undefined,
-    longitude: undefined,
-  });
+const Container: React.FC = () => {
+  const apiKey = GetGoogleMapApiKey();
+
+  // 位置情報の初期化
+  const [position, setPosition] = useState<Positions | undefined>(undefined);
 
   // 現在の位置情報の取得
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const { latitude, longitude } = position.coords;
-      setPosition({ latitude, longitude });
+    navigator.geolocation.getCurrentPosition((pos: GeolocationPosition) => {
+      setPosition({ lat: pos.coords.latitude, lng: pos.coords.longitude });
     });
   }, []);
 
   // デフォルト値の設定
   const defaultData = {
-    center: {
-      lat: position.latitude,
-      lng: position.longitude,
-    },
-    zoom: 14,
+    center: position,
+    zoom: 16,
   };
+
   console.log(position);
 
   return (
-    <Component>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: GetGoogleMapApiKey() }}
-        defaultCenter={defaultData.center}
-        defaultZoom={defaultData.zoom}
-      />
-    </Component>
+    <Component
+      apiKey={apiKey}
+      defaultCenter={defaultData.center}
+      defaultZoom={defaultData.zoom}
+    />
   );
 };
 
